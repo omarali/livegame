5bebc3628a1cf29566d530e86b2ddb85c704276c
Omar Ali <omarali@users.noreply.github.com>
2020-01-05 02:06:45 -0800

    initial site

---
+++ precache-manifest.8c39f9211b0edc5b2a24605261fec44a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b4ac084d36a31d2f66eb",
+    "url": "/static/js/main.295b866e.chunk.js"
+  },
+  {
+    "revision": "aa700f148196bdfeadd0",
+    "url": "/static/js/2.86e75368.chunk.js"
+  },
+  {
+    "revision": "b4ac084d36a31d2f66eb",
+    "url": "/static/css/main.6a4ec717.chunk.css"
+  },
+  {
+    "revision": "6c588840a3e7cf9baa7c06af6f986a95",
+    "url": "/index.html"
+  }
+];
